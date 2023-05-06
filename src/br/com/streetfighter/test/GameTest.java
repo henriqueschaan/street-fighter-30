d659ef93d57c26d9c2ff091055d4d39cab1667fe
Henrique Schaan <102075535+henriqueschaan@users.noreply.github.com>
2023-05-06 20:34:54 -0300

    Add JUnit test

--- src/br/com/streetfighter/test/GameTest.java
+++ src/br/com/streetfighter/test/GameTest.java
@@ -10,6 +10,25 @@ import br.com.streetfighter.characters.*;
 public class GameTest {
 
 	@Test
+	public void testStartGameForTwoPlayers() {
+		// criação dos jogadores e lutadores
+		Fighter fighter1 = new Ryu("Ryu", 100.0, 10.0, new Special("Hadouken", 50.0));
+		Fighter fighter2 = new Guile("Guile", 100.0, 20.0, new Special("Sonic Boom", 40.0));
+		Player player1 = new Player("Player 1");
+		Player player2 = new Player("Player 2");
+		player1.selectFighter(fighter1);
+		player2.selectFighter(fighter2);
+		
+		// criação do jogo e criação da partida através do método startGameForTwoPlayers
+		Game game = new Game();
+		game.startGameForTwoPlayers(player1, player2);
+		
+		// teste se ambos os jogadores têm um lutador selecionado
+		assertNotNull(player1.getFighter());
+		assertNotNull(player2.getFighter());
+	}
+
+	@Test
 	public void testBothAlive() {
 		// criação dos jogadores e lutadores
 		Fighter fighter1 = new Zangief("Zangief", 100.0, 10.0, new Special("Spinning Piledriver", 50.0));
